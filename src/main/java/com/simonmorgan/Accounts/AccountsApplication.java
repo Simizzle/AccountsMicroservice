0a7c560549defe0626233d045f52a2d8cc515f85
Simizzle <85163420+Simizzle@users.noreply.github.com>
2025-04-07 14:59:02 +0100

    feat: adds contact-info endpoint and properties in application.yaml

--- src/main/java/com/simonmorgan/Accounts/AccountsApplication.java
+++ src/main/java/com/simonmorgan/Accounts/AccountsApplication.java
@@ -1,15 +1,18 @@
 package com.simonmorgan.Accounts;
 
+import com.simonmorgan.Accounts.dto.AccountsContactDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactDto.class})
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Accounts microservices REST API Documentation",
--- src/main/java/com/simonmorgan/Accounts/controller/AccountsController.java
+++ src/main/java/com/simonmorgan/Accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.simonmorgan.Accounts.controller;
 
 import com.simonmorgan.Accounts.constants.AccountsConstants;
+import com.simonmorgan.Accounts.dto.AccountsContactDto;
 import com.simonmorgan.Accounts.dto.CustomerDto;
 import com.simonmorgan.Accounts.dto.ErrorResponseDto;
 import com.simonmorgan.Accounts.dto.ResponseDto;
@@ -42,6 +43,9 @@ public class AccountsController {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    AccountsContactDto accountsContactDto;
 
     @Operation(
             summary = "Create Account REST API",
@@ -204,5 +208,29 @@ public class AccountsController {
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @Operation(
+            summary = "Get contact info",
+            description = "Contact info details that can be reached in case of any issues"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @
+                            Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactDto> getContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(accountsContactDto);
     }
 }
